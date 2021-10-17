c1f8d4905584469a5d9a5a866d9fcae6598700b2
Angelo Rafael <angelorpt@gmail.com>
2021-10-17 16:53:22 -0400

    add | Adicionando a responsabilidade ao Service e retirando da Rota

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import CreateCategoryService from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -12,14 +13,8 @@ categoriesRoutes.get("/", (req, res) => {
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists) {
-    return res
-      .status(400)
-      .json({ error: "Já existe uma categoria com este nome" });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   return res.status(201).send();
 });
